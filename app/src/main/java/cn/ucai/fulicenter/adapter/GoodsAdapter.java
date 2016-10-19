4150a6a1d863998f5e9d3ac9923bd332ad6cdaf2
huangdachui1991 <604754456@qq.com>
2016-10-19 21:31:18 +0800

    对照老师的代码解决了bug，完成day18的内容

---
+++ app/src/main/java/cn/ucai/fulicenter/adapter/GoodsAdapter.java
@@ -0,0 +1,134 @@
+package cn.ucai.fulicenter.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.NewGoodsBean;
+import cn.ucai.fulicenter.utils.ImageLoader;
+import cn.ucai.fulicenter.utils.MFGT;
+
+/**
+ * Created by clawpo on 2016/10/17.
+ */
+
+public class GoodsAdapter extends Adapter {
+    Context mContext;
+    List<NewGoodsBean> mList;
+    boolean isMore;
+
+    public GoodsAdapter(Context context, List<NewGoodsBean> list) {
+        mContext = context;
+        mList = new ArrayList<>();
+        mList.addAll(list);
+    }
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ViewHolder holder = null;
+        if (viewType == I.TYPE_FOOTER) {
+            holder = new FooterViewHolder(View.inflate(mContext, R.layout.item_footer, null));
+        } else {
+            holder = new GoodsViewHolder(View.inflate(mContext, R.layout.item_goods, null));
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if(getItemViewType(position)==I.TYPE_FOOTER){
+            FooterViewHolder vh = (FooterViewHolder) holder;
+            vh.mTvFooter.setText(getFootString());
+        }else{
+            GoodsViewHolder vh = (GoodsViewHolder) holder;
+            NewGoodsBean goods = mList.get(position);
+            ImageLoader.downloadImg(mContext,vh.mIvGoodsThumb,goods.getGoodsThumb());
+            vh.mTvGoodsName.setText(goods.getGoodsName());
+            vh.mTvGoodsPrice.setText(goods.getCurrencyPrice());
+            vh.mLayoutGoods.setTag(goods.getGoodsId());
+        }
+    }
+
+    private int getFootString() {
+        return isMore?R.string.load_more:R.string.no_more;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size() + 1 : 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        }
+        return I.TYPE_ITEM;
+    }
+
+    public void initData(ArrayList<NewGoodsBean> list) {
+        if(mList!=null){
+            mList.clear();
+        }
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public void addData(ArrayList<NewGoodsBean> list) {
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class GoodsViewHolder extends ViewHolder{
+        @BindView(R.id.ivGoodsThumb)
+        ImageView mIvGoodsThumb;
+        @BindView(R.id.tvGoodsName)
+        TextView mTvGoodsName;
+        @BindView(R.id.tvGoodsPrice)
+        TextView mTvGoodsPrice;
+        @BindView(R.id.layout_goods)
+        LinearLayout mLayoutGoods;
+
+        GoodsViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+        @OnClick(R.id.layout_goods)
+        public void onGoodsItemClick(){
+            int goodsId = (int) mLayoutGoods.getTag();
+            MFGT.gotoGoodsDetailsActivity(mContext,goodsId);
+        }
+    }
+
+    static class FooterViewHolder extends ViewHolder{
+        @BindView(R.id.tvFooter)
+        TextView mTvFooter;
+
+        FooterViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
