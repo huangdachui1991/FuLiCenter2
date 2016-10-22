ba09011b717326db97e9887e421aa703a37f568c
huangdachui1991 <604754456@qq.com>
2016-10-23 03:28:51 +0800

    分类首页

---
+++ app/src/main/java/cn/ucai/fulicenter/adapter/CategoryAdapter.java
@@ -0,0 +1,146 @@
+package cn.ucai.fulicenter.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.CategoryChildBean;
+import cn.ucai.fulicenter.bean.CategoryGroupBean;
+import cn.ucai.fulicenter.utils.ImageLoader;
+
+/**
+ * Created by huangdachui on 2016/10/23.
+ */
+
+public class CategoryAdapter extends BaseExpandableListAdapter {
+    Context mContext;
+    ArrayList<CategoryGroupBean> mGroupList;
+    ArrayList<ArrayList<CategoryChildBean>> mChildList;
+
+    public CategoryAdapter(Context Context,
+                           ArrayList<CategoryGroupBean> GroupList,
+                           ArrayList<ArrayList<CategoryChildBean>> ChildList) {
+        mContext = Context;
+        mGroupList = new ArrayList<>();
+        mGroupList.addAll(GroupList);
+        mChildList = new ArrayList<>();
+        mChildList.addAll(ChildList);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return mGroupList != null ? mGroupList.size() : 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return mChildList != null && mChildList.get(groupPosition) != null
+                ? mChildList.get(groupPosition).size() : 0;
+    }
+
+    @Override
+    public CategoryGroupBean getGroup(int groupPosition) {
+        return mGroupList != null ? mGroupList.get(groupPosition) : null;
+    }
+
+    @Override
+    public CategoryChildBean getChild(int groupPosition, int childPosition) {
+        return mChildList != null && mChildList.get(groupPosition) != null
+                ? mChildList.get(groupPosition).get(childPosition) : null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View view, ViewGroup viewGroup) {
+        GroupiewHolder holder;
+        if (view == null) {
+            view = View.inflate(mContext, R.layout.item_category_group, null);
+            holder = new GroupiewHolder(view);
+            view.setTag(holder);
+        } else {
+            view.getTag();
+            holder = (GroupiewHolder) view.getTag();
+        }
+        CategoryGroupBean group = getGroup(groupPosition);
+        if (group != null) {
+            ImageLoader.downloadImg(mContext, holder.mivGroupThumb, group.getImageUrl());
+            holder.mtvGroupName.setText(group.getName());
+            holder.mivIndicator.setImageResource(isExpanded ? R.mipmap.expand_off : R.mipmap.expand_on);
+
+        }
+        return view;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View view, ViewGroup viewGroup) {
+        ChildViewHolder holder;
+        if (view == null) {
+            view = View.inflate(mContext, R.layout.item_category_child, null);
+            holder = new ChildViewHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (ChildViewHolder) view.getTag();
+        }
+        CategoryChildBean child = getChild(groupPosition, childPosition);
+
+        if(child!=null){
+            ImageLoader.downloadImg(mContext, holder.mivCategoryChildThumb, child.getImageUrl());
+            holder.mtvCategoryChildname.setText(child.getName());
+        }
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+    class GroupiewHolder {
+        @BindView(R.id.iv_group_thumb)
+        ImageView mivGroupThumb;
+        @BindView(R.id.tv_group_name)
+        TextView mtvGroupName;
+        @BindView(R.id.iv_indicator)
+        ImageView mivIndicator;
+
+        GroupiewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    static class ChildViewHolder {
+        @BindView(R.id.iv_category_child_thumb)
+        ImageView mivCategoryChildThumb;
+        @BindView(R.id.tv_category_childname)
+        TextView mtvCategoryChildname;
+        @BindView(R.id.layout_category_child)
+        RelativeLayout mlayoutCategoryChild;
+
+        ChildViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
